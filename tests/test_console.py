144ee4534a080d338cd664644de5a79da4484a0c
The Primotion Studio <oyedelenewton@gmail.com>
2024-02-11 01:14:04 +0100

    console works

--- tests/test_console.py
+++ tests/test_console.py
@@ -24,14 +24,14 @@ class TestHBNBCommand(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_help_command(self, mock_stdout):
-    """Test module for HBNBCommand class"""
+        """Test module for HBNBCommand class"""
         with patch('builtins.input', return_value='quit'):
             expected_output = (
-                    "Documented commands (type help <topic>):\n"
-                    "========================================\n"
-                    "EOF  all  count  create  destroy  help"
-                    "  quit  show  update"
-                    )
+                "Documented commands (type help <topic>):\n"
+                "========================================\n"
+                "EOF  all  count  create  destroy  help"
+                "  quit  show  update"
+            )
             HBNBCommand().onecmd("help")
             self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
@@ -152,9 +152,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create BaseModel")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_user_command(self, mock_stdout):
@@ -162,9 +161,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create User")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_place_command(self, mock_stdout):
@@ -172,9 +170,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create Place")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_city_command(self, mock_stdout):
@@ -182,9 +179,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create City")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_state_command(self, mock_stdout):
@@ -192,9 +188,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create State")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_review_command(self, mock_stdout):
@@ -202,9 +197,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create Review")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_create_amenity_command(self, mock_stdout):
@@ -212,9 +206,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create Amenity")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_show_command(self, mock_stdout):
@@ -222,9 +215,8 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('builtins.input', return_value='quit'):
             HBNBCommand().onecmd("create BaseModel")
             output = mock_stdout.getvalue().strip()
-            self.assertRegex(output,
-                             r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]\
-                             {4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+            self.assertRegex(
+                output, r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_update_command(self, mock_stdout):
@@ -276,17 +268,6 @@ class TestHBNBCommand(unittest.TestCase):
         correct = "** class doesn't exist **"
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("create MyModel"))
-            self.assertEqual(correct, output.getvalue().strip())
-
-    def test_create_invalid_syntax(self):
-        """Test module for HBNBCommand class"""
-        correct = "*** Unknown syntax: MyModel.create()"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("MyModel.create()"))
-            self.assertEqual(correct, output.getvalue().strip())
-        correct = "*** Unknown syntax: BaseModel.create()"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("BaseModel.create()"))
             self.assertEqual(correct, output.getvalue().strip())
 
 
